0b25ac45b0e983cbb6af45a528eecf009cc55bef
Andrew McCormick <ajm673@mail.umsl.edu>
2019-05-09 16:32:12 -0500

    f3 done

--- main.c
+++ main.c
@@ -18,7 +18,7 @@ int main()
 	
 	//f1();
 
-	f2();
+	//f2();
 
 	f3();
 
@@ -69,5 +69,25 @@ void f2()
 
 void f3()
 {
+	char *charArr = malloc(sizeof(char) * CHARSIZE);
 
+	char c;
+
+	static int n = 0;
+
+	static long int addr = 0;
+
+	n++;
+
+	printf("Call #%d     at %p\n", n, &c);
+	printf("AR Size     #%d is %1d\n", n, addr - (long int)&c);
+	
+	addr = (long int)&c;
+	
+	if(n < 10)
+	{
+		f3();
+	}
+
+	free(charArr);
 }
